619f5fd64fad823cac6644813d3b2f8b55d9c6b3
Cristiane Mayara <cris.mayara@gmail.com>
2018-09-06 15:44:46 -0300

    Using hooks

--- tests/counter.spec.js
+++ tests/counter.spec.js
@@ -2,27 +2,41 @@ const assert = require("assert");
 const counter = require("../src/counter");
 
 describe("Testando contador", function() {
+  before(function() {
+    console.log("before");
+  });
+
+  after(function() {
+    console.log("after");
+  });
+
+  beforeEach(function() {
+    console.log("beforeEach");
+    counter.count = 0;
+  });
+
+  afterEach(function() {
+    console.log("afterEach");
+  });
+
   it("contador deve iniciar com valor zero", function() {
     const counterResult = counter.count;
     assert.equal(counterResult, 0);
   });
 
   it("teste de incremento de 1", function() {
-    counter.count = 0;
     counter.increment();
     const counterResult = counter.count;
     assert.equal(counterResult, 1);
   });
 
   it("teste de decremento de 1", function() {
-    counter.count = 0;
     counter.decrement();
     const counterResult = counter.count;
     assert.equal(counterResult, -1);
   });
 
   it("teste de incremento de 10", function() {
-    counter.count = 0;
     counter.increment(10);
     const counterResult = counter.count;
     assert.equal(counterResult, 10);
