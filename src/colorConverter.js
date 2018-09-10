5be5de84da333553f07584015a3070e91090702c
Cristiane Mayara <cris.mayara@gmail.com>
2018-09-09 21:40:01 -0300

    Converting from RGB to hexadecimal

--- src/colorConverter.js
+++ src/colorConverter.js
@@ -1,5 +1,13 @@
 module.exports = {
   toHex: function(rgb) {
-    return "#ff0000";
+    let red = rgb[0].toString(16);
+    let green = rgb[1].toString(16);
+    let blue = rgb[2].toString(16);
+
+    if (red.length == 1) red = 0 + red;
+    if (green.length == 1) green = 0 + green;
+    if (blue.length == 1) blue = 0 + blue;
+
+    return `#` + red + green + blue;
   }
 };
