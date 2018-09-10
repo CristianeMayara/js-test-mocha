1a01c448e0ccf92745784e6689d9346d4a60b888
Cristiane Mayara <cris.mayara@gmail.com>
2018-09-09 21:43:05 -0300

    Adding tests for green and blue colors

--- tests/color-converter.spec.js
+++ tests/color-converter.spec.js
@@ -8,5 +8,17 @@ describe("Testando conversor de cores", function() {
       const colorHex = colorConverter.toHex(colorRgb);
       assert.equal(colorHex, "#ff0000");
     });
+
+    it("Converte verde", function() {
+      const colorRgb = [0, 255, 0];
+      const colorHex = colorConverter.toHex(colorRgb);
+      assert.equal(colorHex, "#00ff00");
+    });
+
+    it("Converte azul", function() {
+      const colorRgb = [0, 0, 255];
+      const colorHex = colorConverter.toHex(colorRgb);
+      assert.equal(colorHex, "#0000ff");
+    });
   });
 });
