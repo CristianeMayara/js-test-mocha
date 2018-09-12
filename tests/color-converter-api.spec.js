f1473eceffdc1c84d33aeb6a340c1d16dcd383d1
Cristiane Mayara <cris.mayara@gmail.com>
2018-09-12 01:17:31 -0300

    Adding API

--- tests/color-converter-api.spec.js
+++ tests/color-converter-api.spec.js
@@ -3,7 +3,7 @@ const assert = require("assert");
 
 describe("Teste de api de conversao de cores", function() {
   it("Converte para hex", function(done) {
-    request("http://localhost:9000/rgbToHex/255/0/0", function(error, response, body) {
+    request("http://localhost:9000/rgb-to-hex/255/0/0", function(error, response, body) {
       assert.equal(response.statusCode, 200);
       assert.equal(body, "#ff0000");
       done();
@@ -11,9 +11,9 @@ describe("Teste de api de conversao de cores", function() {
   });
 
   it("Converte para rgb", function(done) {
-    request("http://localhost:9000/rgbToRgb/ff0000", function(error, response, body) {
+    request("http://localhost:9000/hex-to-rgb/ff0000", function(error, response, body) {
       assert.equal(response.statusCode, 200);
-      assert.equal(body, "255, 0, 0");
+      assert.equal(body, "255,0,0");
       done();
     });
   });
